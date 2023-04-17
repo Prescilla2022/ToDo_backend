0cdba2e16575a77e1a9aac964f442021008e6700
PrescillaKodehode <113029569+Prescilla2022@users.noreply.github.com>
2023-04-17 12:35:33 +0200

    Update deleteToDoController.js

--- controllers/deleteToDoController.js
+++ controllers/deleteToDoController.js
@@ -3,7 +3,7 @@ const ToDo = require("../model/ToDo");
 const handleDeleteToDo = async (req, res) => {
   console.log(req.body);
 
-  const result = await ToDo.remove({});
+  const result = await ToDo.deleteMany({});
   console.log(result);
 
   return res.status(201).json({ success: `All items deleted` });
