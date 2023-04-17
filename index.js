b123dc53d046183d04e0fd2f0a6be2ab25274d45
PrescillaKodehode <113029569+Prescilla2022@users.noreply.github.com>
2023-04-17 10:52:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const app = express();
 require("dotenv").config();
 const connectDB = require("./config/dbConn");
 connectDB();
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 10000;
 app.use(express.json());
 app.use(cors());
+app.get("/",(req,res)=>{
+  res.setHeader("Access-Control-Allow-Credentials","true"),
+    res.send("Api running");
+}
+  
 
 app.use("/show", require("./routes/showToDo"));
 app.use("/add", require("./routes/addToDo"));
